c7527c39cbb1d44320e8e6fd6ddd6c5d655d602f
Jay Davis <jdavis@vandegriftinc.com>
2020-10-18 23:00:53 -0400

    Update dependencies (libgfortran3 may need to be installed manually)

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "https://github.com/clojuredatascience/ch3-correlation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [incanter/incanter "1.5.6"]
-                 [clj-time "0.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [incanter/incanter "1.5.7"]
+                 [clj-time "0.15.2"]]
   :resource-paths ["data"]
   :aot [cljds.ch3.core]
   :main cljds.ch3.core
   :repl-options {:init-ns cljds.ch3.examples}
-  :profiles {:dev {:dependencies [[org.clojure/tools.cli "0.3.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/tools.cli "1.0.194"]]}})
